8e30b36247851f37e0bbbaddaa57c0f6527c37c5
Natascha Lamsu <tazcha15@gmail.com>
2018-03-23 08:50:25 +0700

    mvc sequelize migration seed

---
+++ view.js
@@ -0,0 +1,34 @@
+class View {
+  static viewList(data) {
+    console.log(data);
+  }
+  static addContact(data) {
+    console.log(data);
+  }
+  static updateContact(data) {
+    console.log(data);
+  }
+  static deleteContact(data) {
+    console.log(data);
+  }
+  static addAddress(data) {
+    console.log(data);
+  }
+  static showAddresses(data) {
+    console.log(data);
+  }
+  static viewAddresses(data) {
+    console.log(data);
+  }
+  static updateAddress(data) {
+    console.log(data);
+  }
+  static deleteAddress(data) {
+    console.log(data);
+  }
+  // static showAddresses(data) {
+  //   console.log(data);
+  // }
+}
+
+module.exports = View
