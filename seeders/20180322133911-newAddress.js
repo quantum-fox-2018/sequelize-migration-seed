2968ffb9386ededd836f42c140434e6d70367481
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-03-23 17:43:47 +0700

    nambah addres

--- seeders/20180322133911-newAddress.js
+++ seeders/20180322133911-newAddress.js
@@ -1,78 +1,36 @@
 'use strict';
+const fs = require('fs');
+let address = fs.readFileSync('./addresses.csv','utf8').split('\n');
+let detailData=[]
+for (var i = 0; i < address.length-1; i++) {
+  let data = address[i].split(',')
+  let objData={ street:data[1],
+                city:data[2],
+                zip_code:data[3],
+                createdAt: new Date(),
+                updatedAt: new Date()
+                };
+  detailData.push(objData)
+}
 
+
+//console.log(detailData);
+
+
+
+
+
+console.log(address);
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Addresses', [{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },{
-      street: 'Keegan Coleman',
-      city: 'leo.Cras.vehicula@musProinvel.edu',
-      zip_code: '1-998-626-8896',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    return queryInterface.bulkInsert('Addresses',detailData , {});
 
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+      return queryInterface.bulkInsert('Person', [], {});
     */
   },
 
